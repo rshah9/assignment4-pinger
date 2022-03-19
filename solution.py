c415b5efa7a6a144218e6515fe0ba785fb9a3262
rshah9 <rgene2@gmail.com>
2022-03-19 10:23:45 -0400

    git commit test

--- solution.py
+++ solution.py
@@ -50,6 +50,13 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         # Fill in start
 
         # Fetch the ICMP header from the IP packet
+        type, code, checksum, packetID, sequence = struct.unpack(
+            "bbHHh", recPacket[20:28])
+        if packetID == ID:
+            bytesInDouble = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+            return timeReceived - timeSent
+
 
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
