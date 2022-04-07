1e8cd2ea0302893592c03ad7539a0c353e920b40
rshah9 <rgene2@gmail.com>
2022-04-07 17:57:08 -0400

    fix indent

--- solution.py
+++ solution.py
@@ -53,10 +53,11 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         icmpHeader = recPacket[20:28]
         type, code, checksum, id, sequence = struct.unpack(
             "bbHHh", icmpHeader)
-        if type == 0 and id == ID:
+        if id == ID:
             bytesInDouble = struct.calcsize("d")
             timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
             return timeReceived - timeSent
+
 #         if type != 0:
 #             return 'expected type=0, but got {}'.format(type)
 #         if code != 0:
